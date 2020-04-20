97a0938e8086aadd913d51ff92a5a5f44054f5c9
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-04-20 22:18:57 +0200

    Wavelength calibration with Q+-

--- ispex/general.py
+++ ispex/general.py
@@ -2,3 +2,16 @@
 General functions for iSPEX 2 data reduction
 """
 
+import numpy as np
+
+def find_spectrum(data, device="iPhone SE"):
+    """
+    Find the x and y limits that contain the two spectra in the image
+
+    Hardcoded for now
+    """
+
+    slice_Qp = np.s_[540:1400]
+    slice_Qm = np.s_[1480:2300]
+
+    return slice_Qp, slice_Qm
--- wavelength_calibration.py
+++ wavelength_calibration.py
@@ -15,76 +15,83 @@ will be fixed with the general overhaul for iSPEX 2.
 
 import numpy as np
 from sys import argv
-from spectacle import general, io, plot, wavelength, raw2, calibrate, flat
-from ispex import general, wavelength as wvl
+from spectacle import general, io, plot, wavelength, raw2
+from ispex import general as ispex_general, wavelength as wvl
+from pathlib import Path
+from matplotlib import pyplot as plt
 
 # Get the data folder from the command line
 file = io.path_from_input(argv)
-root = io.find_root_folder(file)
-save_to = root/"intermediaries/spectral_response/ispex_wavelength_solution.npy"
+save_to = Path("calibration_data")/"wavelength_calibration.npy"
 
 # Load the data
 img = io.load_raw_file(file)
 print("Loaded data")
 
-# Bias correction
-values = calibrate.correct_bias(root, img.raw_image)
+data = img.raw_image.astype(np.float64)
+bayer_map = img.raw_colors
 
-# Flat-field correction (includes clipping data)
-values = calibrate.correct_flatfield(root, values)
+# Rudimentary bias calibration
+data = data - float(img.black_level_per_channel[0])
 
-# Clip the Bayer map to be the same shape
-bayer_map = flat.clip_data(img.raw_colors)
+slice_Qp, slice_Qm = ispex_general.find_spectrum(data)
 
-# Cut out the spectrum
-# Note that these limits are hard-coded
-xmin, xmax = 1900, 3500
-ymin, ymax = 510, 1220
-cut = np.s_[ymin:ymax, xmin:xmax]
-image_cut  = values[cut]
-colors_cut = bayer_map[cut]
-x = np.arange(xmin, xmax)
-y = np.arange(ymin, ymax)
+data_Qp, data_Qm = data[slice_Qp], data[slice_Qm]
+bayer_Qp, bayer_Qm = bayer_map[slice_Qp], bayer_map[slice_Qm]
 
-# Split the image into RBG (not RGBG2!) using the new pull_apart method
-RGB = raw2.pull_apart2(image_cut, colors_cut)
-plot.show_RGBG(RGB)
+RGB_Qp = raw2.pull_apart2(data_Qp, bayer_Qp)
+RGB_Qm = raw2.pull_apart2(data_Qm, bayer_Qm)
+
+x = np.arange(RGB_Qp.shape[2])
+yp = np.arange(RGB_Qp.shape[1])
+ym = np.arange(RGB_Qm.shape[1])
 
 # Convolve the data with a Gaussian kernel on the wavelength axis to remove
 # noise and fill in the gaps
-RGB_gauss = general.gauss_nan(RGB, sigma=(0,0,10))
-
-# Show the Gaussed image
-plot.show_RGBG(RGB_gauss)
+gauss_Qp = general.gauss_nan(RGB_Qp, sigma=(0,0,10))
+gauss_Qm = general.gauss_nan(RGB_Qm, sigma=(0,0,10))
 
 # Find the locations of the line peaks in every row
-lines = wavelength.find_fluorescent_lines(RGB_gauss) + xmin
+x_offset = 1700
+lines_Qp = wavelength.find_fluorescent_lines(gauss_Qp[...,x_offset:]) + x_offset
+lines_Qm = wavelength.find_fluorescent_lines(gauss_Qm[...,x_offset:]) + x_offset
 
-# Fit a parabola (smile) to the found positions
-lines_fit = wavelength.fit_fluorescent_lines(lines, y)
+lines_fit_Qp = wavelength.fit_fluorescent_lines(lines_Qp, yp)
+lines_fit_Qm = wavelength.fit_fluorescent_lines(lines_Qm, ym)
 
-# Plot the observed and fitted line positions
-plot.plot_fluorescent_lines(y, lines, lines_fit)
+plot.plot_fluorescent_lines(yp, lines_Qp, lines_fit_Qp)
+plot.plot_fluorescent_lines(ym, lines_Qm, lines_fit_Qm)
 
 # Fit a wavelength relation for each row
-wavelength_fits = wavelength.fit_many_wavelength_relations(y, lines_fit)
+wavelength_fits_Qp = wavelength.fit_many_wavelength_relations(yp, lines_fit_Qp)
+wavelength_fits_Qm = wavelength.fit_many_wavelength_relations(ym, lines_fit_Qm)
 
 # Fit a polynomial to the coefficients of the previous fit
-coefficients, coefficients_fit = wavelength.fit_wavelength_coefficients(y, wavelength_fits)
+coefficients_Qp, coefficients_fit_Qp = wavelength.fit_wavelength_coefficients(yp, wavelength_fits_Qp)
+coefficients_Qm, coefficients_fit_Qm = wavelength.fit_wavelength_coefficients(ym, wavelength_fits_Qm)
 
 # Plot the polynomial fits to the coefficients
-plot.wavelength_coefficients(y, wavelength_fits, coefficients_fit)
+plot.wavelength_coefficients(yp, wavelength_fits_Qp, coefficients_fit_Qp)
+plot.wavelength_coefficients(ym, wavelength_fits_Qm, coefficients_fit_Qm)
 
 # Save the coefficients to file
-wavelength.save_coefficients(coefficients, saveto=save_to)
+wavelength.save_coefficients(coefficients_Qp, saveto=save_to)
+wavelength.save_coefficients(coefficients_Qm, saveto=save_to)
 print(f"Saved wavelength coefficients to '{save_to}'")
 
 # Convert the input spectrum to wavelengths and plot it, as a sanity check
-wavelengths_cut = wavelength.calculate_wavelengths(coefficients, x, y)
-wavelengths_split,_ = raw2.pull_apart(wavelengths_cut, colors_cut)
-RGBG,_ = raw2.pull_apart(image_cut, colors_cut)
+wavelengths_Qp = wavelength.calculate_wavelengths(coefficients_Qp, x, yp)
+wavelengths_Qm = wavelength.calculate_wavelengths(coefficients_Qm, x, ym)
 
-lambdarange, all_interpolated = wavelength.interpolate_multi(wavelengths_split, RGBG)
+wavelengths_split_Qp,_ = raw2.pull_apart(wavelengths_Qp, bayer_Qp)
+wavelengths_split_Qm,_ = raw2.pull_apart(wavelengths_Qm, bayer_Qm)
+RGBG_Qp,_ = raw2.pull_apart(data_Qp, bayer_Qp)
+RGBG_Qm,_ = raw2.pull_apart(data_Qm, bayer_Qm)
 
-stacked = wavelength.stack(lambdarange, all_interpolated)
-plot.plot_fluorescent_spectrum(stacked[0], stacked[1:])
+lambdarange, all_interpolated_Qp = wavelength.interpolate_multi(wavelengths_split_Qp, RGBG_Qp)
+lambdarange, all_interpolated_Qm = wavelength.interpolate_multi(wavelengths_split_Qm, RGBG_Qm)
+
+stacked_Qp = wavelength.stack(lambdarange, all_interpolated_Qp)
+stacked_Qm = wavelength.stack(lambdarange, all_interpolated_Qm)
+plot.plot_fluorescent_spectrum(stacked_Qp[0], stacked_Qp[1:])
+plot.plot_fluorescent_spectrum(stacked_Qm[0], stacked_Qm[1:])
